c80e8762dcb12a080aa842ccf3f61b3460eb885f
Alexander <alexander.wang2001@gmail.com>
2016-06-12 16:02:20 -0700

    Add the main program files

---
+++ main.js
@@ -0,0 +1,41 @@
+/*
++====================+
+   Global Variables
++====================+
+*/
+var screen = {
+    h: 400,
+    w: 400,
+}
+/*
++====================+
+   Canvas Creation
++====================+
+*/
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+canvas.height = screen.h;
+canvas.width = screen.w;
+document.body.appendChild(canvas);
+
+/*
++====================+
+   Various Methods
++====================+
+*/
+/*
++====================+
+        Update
++====================+
+*/
+/*
++====================+
+        Render
++====================+
+*/
+/*
++====================+
+  Finalize and Start
++====================+
+*/
